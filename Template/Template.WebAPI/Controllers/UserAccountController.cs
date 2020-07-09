381e874ade13afdcbaa05a00737eb6df31c6c40b
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-07-09 16:01:19 +0200

    Rename Template.Model to Template.Contracts

--- Template/Template.WebAPI/Controllers/UserAccountController.cs
+++ Template/Template.WebAPI/Controllers/UserAccountController.cs
@@ -22,7 +22,29 @@ namespace Template.WebAPI.Controllers
         [HttpPost(ApiRoutes.UserAccount.Register)]
         public async Task<AuthenticationResult> Register([FromBody] UserAccountRegistrationRequest request)
         {
-            return await _userAccountService.RegisterAsync(request);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = ModelState.Values.SelectMany(x => x.Errors.Select(xx => xx.ErrorMessage))
+                });
+            }
+
+            var authResponse = await _identityService.RegisterAsync(request.Email, request.Password);
+
+            if (!authResponse.Success)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = authResponse.Errors
+                });
+            }
+
+            return Ok(new AuthSuccessResponse
+            {
+                Token = authResponse.Token,
+                RefreshToken = authResponse.RefreshToken
+            });
         }
 
         [HttpPost(ApiRoutes.UserAccount.Authenticate)]
