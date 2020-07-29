f415fddcaf77611a05a3f2aab55bbc1cde89634d
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-07-29 15:53:20 +0200

    Move AuthResult to Domain

--- Template/Template.Domain/AuthenticationResult.cs
+++ Template/Template.Domain/AuthenticationResult.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Template.Database
+namespace Template.Domain
 {
     public class AuthenticationResult
     {
--- Template/Template.WebAPI/Services/Interfaces/IUserAccountService.cs
+++ Template/Template.WebAPI/Services/Interfaces/IUserAccountService.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Template.Contracts.Requests;
-using Template.Contracts.Responses;
-using Template.Database;
+using Template.Domain;
 
 namespace Template.WebAPI.Services.Interfaces
 {
