81ce5cd4df49aaee08f67d18d9e6d69f6e2ef12e
Enis Mulić <enis.s.mulic@gmail.com>
2020-12-17 21:07:18 +0100

    Add an extension to get a user id from the httpcontext

---
+++ Template.WebAPI/Extensions/GeneralExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace Template.WebAPI.Extensions
+{
+    public static class GeneralExtensions
+    {
+        public static string GetUserId(this HttpContext httpContext)
+        {
+            if (httpContext.User == null)
+            {
+                return string.Empty;
+            }
+
+            return httpContext.User.Claims.Single(x => x.Type == "id").Value;
+        }
+    }
+}
