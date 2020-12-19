219eec76e77ab0e9e3fe4e33476deb7c86c81b80
Enis Mulić <enis.s.mulic@gmail.com>
2020-12-19 18:43:03 +0100

    Add sdk

---
+++ Template.Sdk/ITemplateApi.cs
@@ -0,0 +1,25 @@
+﻿using Refit;
+using System.Threading.Tasks;
+using Template.Contracts.V1.Requests;
+using Template.Contracts.V1.Responses;
+
+namespace Template.Sdk
+{
+    public interface ITemplateApi
+    {
+        [Get("/api/User")]
+        Task<ApiResponse<PagedResponse<UserResponse>>> GetUsersAsync(UserSearchRequest request = default, PaginationQuery pagination = default);
+        
+        [Get("/api/User/{id}")]
+        Task<ApiResponse<UserResponse>> GetUserByIdAsync(int id);
+
+        [Put("/api/User/{id}")]
+        Task<ApiResponse<UserResponse>> UpdateUserAsync(int id, UserUpdateRequest request);
+
+        [Delete("/api/User/{id}")]
+        Task<ApiResponse<bool>> DeleteKorisnikAsync(int id);
+
+        [Get("/api/Role")]
+        Task<ApiResponse<PagedResponse<RoleResponse>>> GetRolesAsync();
+    }
+}
