df8a3b237695b8c1ac51984fa09fcf6a913cde16
Enis Mulić <enis.s.mulic@gmail.com>
2020-12-23 12:34:39 +0100

    Add and configure email service

---
+++ Template.EmailService/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Template.EmailService
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(IEnumerable<string> to, string subject, string content, IFormFileCollection attachments);
+        Task SendEmailAsync(IEnumerable<string> to, string subject, string content);
+    }
+}
---
+++ Template.WebAPI/Installers/EmailInstaller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Template.EmailService;
+
+namespace Template.WebAPI.Installers
+{
+    public class EmailInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            var emailConfig = configuration
+                .GetSection("EmailConfiguration")
+                .Get<EmailConfiguration>();
+
+            services.AddSingleton(emailConfig);
+
+            services.AddScoped<IEmailSender, EmailSender>();
+        }
+    }
+}
